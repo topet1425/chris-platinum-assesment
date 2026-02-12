61610692155f1326d15ff8efb3d63ed45b06b9e7
Christopher Marqueses <topetmarqueses25@gmail.com>
2026-02-12 20:54:44 +0800

    update tailwind version to v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+
+import Manage from "./pages/Manage";
+import Users from "./pages/Users";
+import Tags from "./pages/Tags";
+import Integrations from "./pages/Integrations";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className="flex h-screen">
+        <Navbar />
+        <div className="flex-1 flex flex-col">
+          <Header />
+          <div className="p-6 overflow-auto">
+            <Routes>
+              <Route path="/" element={<Manage />} />
+              <Route path="/users" element={<Users />} />
+              <Route path="/tags" element={<Tags />} />
+              <Route path="/integrations" element={<Integrations />} />
+            </Routes>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
